00178789a5c698cc3eb6d775c3669e56b403f33a
takusan23 <takumu1200@gmail.com>
2023-02-26 03:36:19 +0900

    onFrameAvailableに書いていた処理を別スレッドに移動

--- app/src/main/java/io/github/takusan23/wipecamera/Camera.kt
+++ app/src/main/java/io/github/takusan23/wipecamera/Camera.kt
@@ -8,37 +8,66 @@ import android.hardware.camera2.CameraManager
 import android.hardware.camera2.params.OutputConfiguration
 import android.hardware.camera2.params.SessionConfiguration
 import android.view.Surface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+/** カメラを使ってプレビュー、録画を担当する */
 class Camera(
     context: Context,
     private val cameraId: String,
-    private val outputSurfaceList: List<Surface>
+    private val previewSurface: Surface,
+    private val recorderSurface: Surface
 ) {
     private val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
     private val cameraExecutor = Executors.newSingleThreadExecutor()
     private var cameraDevice: CameraDevice? = null
 
-    suspend fun setupCamera() {
+    /** カメラを準備する */
+    suspend fun waitOpenCamera() {
         val cameraDevice = waitOpenCamera(cameraId)
         this.cameraDevice = cameraDevice
+    }
 
+    /** カメラをプレビューモードにする。[recorderSurface]には何も映らなくなる */
+    fun startPreview() {
+        val cameraDevice = cameraDevice ?: return
         val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
-            outputSurfaceList.forEach {
-                addTarget(it)
-            }
+            addTarget(previewSurface)
         }.build()
         val outputList = buildList {
-            outputSurfaceList.forEach {
-                add(OutputConfiguration(it))
-            }
+            add(OutputConfiguration(previewSurface))
         }
 
         SessionConfiguration(SessionConfiguration.SESSION_REGULAR, outputList, cameraExecutor, object : CameraCaptureSession.StateCallback() {
             override fun onConfigured(captureSession: CameraCaptureSession) {
                 captureSession.setRepeatingRequest(captureRequest, null, null)
+            }
+
+            override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+            }
+        }).apply { cameraDevice.createCaptureSession(this) }
+    }
+
+    /** カメラを録画モードにする */
+    suspend fun startRecord() = suspendCoroutine { continuation ->
+        val cameraDevice = cameraDevice!!
+        val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD).apply {
+            addTarget(previewSurface)
+            addTarget(recorderSurface)
+        }.build()
+        val outputList = buildList {
+            add(OutputConfiguration(previewSurface))
+            add(OutputConfiguration(recorderSurface))
+        }
+
+        SessionConfiguration(SessionConfiguration.SESSION_REGULAR, outputList, cameraExecutor, object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(captureSession: CameraCaptureSession) {
+                captureSession.setRepeatingRequest(captureRequest, null, null)
+                continuation.resume(Unit)
             }
 
             override fun onConfigureFailed(p0: CameraCaptureSession) {
@@ -54,20 +83,22 @@ class Camera(
 
     /** カメラを開く */
     @SuppressLint("MissingPermission")
-    private suspend fun waitOpenCamera(cameraId: String) = suspendCoroutine { continuation ->
-        cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-            override fun onOpened(camera: CameraDevice) {
-                continuation.resume(camera)
-            }
+    private suspend fun waitOpenCamera(cameraId: String) = withContext(Dispatchers.Main) {
+        suspendCoroutine { continuation ->
+            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+                override fun onOpened(camera: CameraDevice) {
+                    continuation.resume(camera)
+                }
 
-            override fun onDisconnected(camera: CameraDevice) {
+                override fun onDisconnected(camera: CameraDevice) {
+                    // do nothing
+                }
 
-            }
-
-            override fun onError(camera: CameraDevice, p1: Int) {
-
-            }
-        }, null)
+                override fun onError(camera: CameraDevice, p1: Int) {
+                    // do nothing
+                }
+            }, null)
+        }
     }
 
 }
--- app/src/main/java/io/github/takusan23/wipecamera/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/wipecamera/MainActivity.kt
@@ -21,28 +21,43 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
 import io.github.takusan23.wipecamera.databinding.ActivityMainBinding
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.File
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+
 
 class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListener {
 
     private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    /** Surface で描画している OpenGL の管理するやつ */
+    /**
+     * Surface で描画している OpenGL の管理するやつ
+     *
+     * Index 0 = Preview
+     * Index 1 = Recorder
+     */
     private val surfaceInputOpenGlList = mutableListOf<SurfaceInputOpenGl>()
 
     /**
-     * SurfaceTexture の配列
+     * [SurfaceTexture] の配列
+     *
+     * Index 0 = Preview OpenGL バックカメラ
+     * Index 1 = Preview OpenGL フロントカメラ
+     * Index 2 = Record OpenGL バックカメラ
+     * Index 3 = Record OpenGL フロントカメラ
+     */
+    private val surfaceTextureList = mutableListOf<SurfaceTexture>()
+
+    /** [CameraManager] */
+    private val cameraManager by lazy { getSystemService(Context.CAMERA_SERVICE) as CameraManager }
+
+    /**
+     * カメラ
      *
      * Index 0 = バックカメラ
      * Index 1 = フロントカメラ
      */
-    private val surfaceTextureList = mutableListOf<SurfaceTexture>()
-
-    /** カメラ */
-    private val cameraManager by lazy { getSystemService(Context.CAMERA_SERVICE) as CameraManager }
     private val useCameraList = mutableListOf<Camera>()
 
     /** [MediaRecorder] */
@@ -55,9 +70,7 @@ class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListene
     /** 権限リクエストする */
     private val permissionRequester = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
         if (result.all { it.value }) {
-            lifecycleScope.launch {
-                setupCamera()
-            }
+            setupCamera()
         }
     }
 
@@ -81,9 +94,7 @@ class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListene
             ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
             && ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
         ) {
-            lifecycleScope.launch {
-                setupCamera()
-            }
+            setupCamera()
         } else {
             // 権限を求める
             permissionRequester.launch(arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.RECORD_AUDIO))
@@ -91,36 +102,33 @@ class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListene
 
         // 録画する
         viewBinding.recordButton.setOnClickListener {
-            if (!isRecording) {
-                Toast.makeText(this, "録画開始", Toast.LENGTH_SHORT).show()
-                viewBinding.recordButton.setImageResource(R.drawable.outline_stop_24)
-                mediaRecorder.start()
-            } else {
-                Toast.makeText(this, "終了", Toast.LENGTH_SHORT).show()
-                viewBinding.recordButton.setImageResource(R.drawable.outline_videocam_24)
-                mediaRecorder.stop()
-                setMediaRecorderParams(mediaRecorder)
+            lifecycleScope.launch {
+                if (!isRecording) {
+                    Toast.makeText(this@MainActivity, "録画開始", Toast.LENGTH_SHORT).show()
+                    viewBinding.recordButton.setImageResource(R.drawable.outline_stop_24)
+                    startRecord()
+                } else {
+                    Toast.makeText(this@MainActivity, "終了", Toast.LENGTH_SHORT).show()
+                    viewBinding.recordButton.setImageResource(R.drawable.outline_videocam_24)
+                    mediaRecorder.stop()
+                    mediaRecorder.reset()
+                    setMediaRecorderParams(mediaRecorder)
+                }
+                isRecording = !isRecording
             }
-            isRecording = !isRecording
         }
     }
+
+    private var latestUpdate = 0L
 
     /**
      * SurfaceTexture (カメラのフレーム) が更新されたら呼ばれる
      * OpenGL で加工して Surface に流す
      */
     override fun onFrameAvailable(surfaceTexture: SurfaceTexture?) {
-        // プレビュー の Surface へ描画する
-        val (previewGl, recorderGl) = surfaceInputOpenGlList
-        previewGl.makeCurrent()
-        previewGl.drawFrame()
-        previewGl.swapBuffers()
-        // 次に MediaRecorder の Surface に描画する
-        if (isRecording) {
-            recorderGl.makeCurrent()
-            recorderGl.drawFrame()
-            recorderGl.swapBuffers()
-        }
+        // ここは UIスレッド なので重たい処理はしないこと
+        // レンダリングは別スレッドに移動させてあります
+        latestUpdate = System.currentTimeMillis()
     }
 
     override fun onDestroy() {
@@ -131,73 +139,110 @@ class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListene
     }
 
     /** カメラの初期化をする */
-    private suspend fun setupCamera() {
-        // プレビュー用の Surface
-        val previewSurface = waitSurface().surface
-        // 録画用の Surface
-        val recorderSurface = mediaRecorder.surface
+    private fun setupCamera() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            // プレビュー用の Surface
+            val previewSurface = waitSurface().surface
+            // 録画用の Surface
+            val recorderSurface = mediaRecorder.surface
 
-        // Surface の数だけ CameraGlRenderer 作る
-        val previewGlRenderer = CameraGlRenderer(
-            rotation = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 90f else 0f,
-            // プレビュー用に作った SurfaceTexture
-            onRequestBackCameraSurfaceTexture = { surfaceTextureList[0] },
-            onRequestFrontCameraSurfaceTexture = { surfaceTextureList[1] }
-        )
-        val recorderGlRenderer = CameraGlRenderer(
-            rotation = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 90f else 0f,
-            // 録画用に作った SurfaceTexture
-            onRequestBackCameraSurfaceTexture = { surfaceTextureList[2] },
-            onRequestFrontCameraSurfaceTexture = { surfaceTextureList[3] }
-        )
+            // Surface の数だけ CameraGlRenderer 作る
+            val previewGlRenderer = CameraGlRenderer(
+                rotation = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 90f else 0f,
+                // プレビュー用に作った SurfaceTexture
+                onRequestBackCameraSurfaceTexture = { surfaceTextureList[0] },
+                onRequestFrontCameraSurfaceTexture = { surfaceTextureList[1] }
+            )
+            val recorderGlRenderer = CameraGlRenderer(
+                rotation = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 90f else 0f,
+                // 録画用に作った SurfaceTexture
+                onRequestBackCameraSurfaceTexture = { surfaceTextureList[2] },
+                onRequestFrontCameraSurfaceTexture = { surfaceTextureList[3] }
+            )
 
-        // プレビューSurface 録画Surface それぞれ OpenGL のソースを用意する
-        surfaceInputOpenGlList.clear()
-        val previewSurfaceInputOpenGl = SurfaceInputOpenGl(previewSurface, previewGlRenderer)
-        val recorderSurfaceInputOpenGl = SurfaceInputOpenGl(recorderSurface, recorderGlRenderer)
-        surfaceInputOpenGlList.addAll(listOf(previewSurfaceInputOpenGl, recorderSurfaceInputOpenGl))
+            // プレビューSurface 録画Surface それぞれ OpenGL のソースを用意する
+            surfaceInputOpenGlList.clear()
+            val previewSurfaceInputOpenGl = SurfaceInputOpenGl(previewSurface, previewGlRenderer)
+            val recorderSurfaceInputOpenGl = SurfaceInputOpenGl(recorderSurface, recorderGlRenderer)
+            surfaceInputOpenGlList.addAll(listOf(previewSurfaceInputOpenGl, recorderSurfaceInputOpenGl))
 
-        // SurfaceTexture を用意
-        // makeCurrent を呼ぶと GLThread のエラーが消える？
-        previewSurfaceInputOpenGl.makeCurrent()
-        val previewSurfaceTexturePair = previewGlRenderer.setupProgram().let { (backCameraTextureId, frontCameraTextureId) ->
-            // バックカメラ、フロントカメラ それぞれ SurfaceTexture を作成する
-            // SurfaceTexture の場合は setDefaultBufferSize で解像度の設定ができる
-            val back = SurfaceTexture(backCameraTextureId).apply {
-                setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
-                setOnFrameAvailableListener(this@MainActivity)
+            // SurfaceTexture を用意
+            // makeCurrent を呼ぶと GLThread のエラーが消える？
+            previewSurfaceInputOpenGl.makeCurrent()
+            val previewSurfaceTexturePair = previewGlRenderer.setupProgram().let { (backCameraTextureId, frontCameraTextureId) ->
+                // バックカメラ、フロントカメラ それぞれ SurfaceTexture を作成する
+                // SurfaceTexture の場合は setDefaultBufferSize で解像度の設定ができる
+                val back = SurfaceTexture(backCameraTextureId).apply {
+                    setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
+                    setOnFrameAvailableListener(this@MainActivity)
+                }
+                val front = SurfaceTexture(frontCameraTextureId).apply {
+                    setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
+                    setOnFrameAvailableListener(this@MainActivity)
+                }
+                back to front
             }
-            val front = SurfaceTexture(frontCameraTextureId).apply {
-                setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
-                setOnFrameAvailableListener(this@MainActivity)
+
+            recorderSurfaceInputOpenGl.makeCurrent()
+            val recorderSurfaceTexturePair = recorderGlRenderer.setupProgram().let { (backCameraTextureId, frontCameraTextureId) ->
+                val back = SurfaceTexture(backCameraTextureId).apply {
+                    setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
+                    setOnFrameAvailableListener(this@MainActivity)
+                }
+                val front = SurfaceTexture(frontCameraTextureId).apply {
+                    setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
+                    setOnFrameAvailableListener(this@MainActivity)
+                }
+                back to front
             }
-            back to front
+            // リソース開放用に配列に入れておく
+            surfaceTextureList.clear()
+            surfaceTextureList.addAll(previewSurfaceTexturePair.toList() + recorderSurfaceTexturePair.toList())
+
+            // カメラを開いて、配列に入れる
+            val (backCameraId, frontCameraId) = getCameraIdData()
+            useCameraList.clear()
+            useCameraList += Camera(this@MainActivity, backCameraId, Surface(previewSurfaceTexturePair.first), Surface(recorderSurfaceTexturePair.first))
+            useCameraList += Camera(this@MainActivity, frontCameraId, Surface(previewSurfaceTexturePair.second), Surface(recorderSurfaceTexturePair.second))
+            useCameraList.forEach { it.waitOpenCamera() }
+            // プレビューを開始する
+            val (backCamera, frontCamera) = useCameraList
+            backCamera.startPreview()
+            frontCamera.startPreview()
+
+            // OpenGL で Surface にレンダリングをする
+            // なんでここでやっているのかというと
+            // onFrameAvailable が UIスレッド で呼ばれます。
+            // そこに MediaRecorder の重たい処理が入ると UI が固まってしまいます、、、
+            // なので、新しいフレームが来たかどうかを、別スレッドの while ループで確認してUIスレッドを止めないようにしています。
+            var prevUpdate = 0L
+            while (isActive) {
+                // プレビュー の Surface へ描画する
+                if (latestUpdate != prevUpdate) {
+                    val (previewGl, recorderGl) = surfaceInputOpenGlList
+                    previewGl.makeCurrent()
+                    previewGl.drawFrame()
+                    previewGl.swapBuffers()
+                    // 次に MediaRecorder の Surface に描画する
+                    if (isRecording) {
+                        recorderGl.makeCurrent()
+                        recorderGl.drawFrame()
+                        recorderGl.swapBuffers()
+                    }
+                    prevUpdate = latestUpdate
+                }
+            }
         }
+    }
 
-        recorderSurfaceInputOpenGl.makeCurrent()
-        val recorderSurfaceTexturePair = recorderGlRenderer.setupProgram().let { (backCameraTextureId, frontCameraTextureId) ->
-            val back = SurfaceTexture(backCameraTextureId).apply {
-                setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
-                setOnFrameAvailableListener(this@MainActivity)
-            }
-            val front = SurfaceTexture(frontCameraTextureId).apply {
-                setDefaultBufferSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
-                setOnFrameAvailableListener(this@MainActivity)
-            }
-            back to front
-        }
-        // リソース開放用に配列に入れておく
-        surfaceTextureList.clear()
-        surfaceTextureList.addAll(previewSurfaceTexturePair.toList() + recorderSurfaceTexturePair.toList())
-
-        // カメラを開いて、配列に入れる
-        val (backCameraId, frontCameraId) = getCameraIdData()
-        useCameraList.clear()
-        // プレビュー用 SurfaceTexture / 録画用 SurfaceTexture
-        useCameraList += Camera(this@MainActivity, backCameraId, listOf(Surface(previewSurfaceTexturePair.first), Surface(recorderSurfaceTexturePair.first)))
-        useCameraList += Camera(this@MainActivity, frontCameraId, listOf(Surface(previewSurfaceTexturePair.second), Surface(recorderSurfaceTexturePair.second)))
-        // 初期化
-        useCameraList.forEach { it.setupCamera() }
+    /** 録画を開始する */
+    private suspend fun startRecord() = withContext(Dispatchers.Main) {
+        // Camera2 API で録画をする。出力先は MediaRecorder の Surface
+        val (backCamera, frontCamera) = useCameraList
+        listOf(
+            async { backCamera.startRecord() }, async { frontCamera.startRecord() }
+        ).map { it.await() }
+        mediaRecorder.start()
     }
 
     /** Surface の用意が終わるまで一時停止する */
@@ -220,11 +265,11 @@ class MainActivity : AppCompatActivity(), SurfaceTexture.OnFrameAvailableListene
     /** [MediaRecorder]のパラメーターをセットする */
     private fun setMediaRecorderParams(mediaRecorder: MediaRecorder) {
         mediaRecorder.apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setAudioSource(MediaRecorder.AudioSource.CAMCORDER)
             setVideoSource(MediaRecorder.VideoSource.SURFACE)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
             setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
             setVideoEncodingBitRate(1_000_000)
             setVideoFrameRate(30)
             setVideoSize(CAMERA_RESOLTION_WIDTH, CAMERA_RESOLTION_HEIGHT)
